ba3a7a65ce45cdd87416ef6304913672f3c204bb
frederic <fredericseguier@yahoo.fr>
2021-06-14 12:58:23 +0200

    ADD: redux-signin-signup-provider-signout

--- src/index.jsx
+++ src/index.jsx
@@ -1,34 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {
-    BrowserRouter as Router,
-    Route,
-    Switch,
-  } from 'react-router-dom';
-import './index.css'
-import Home from "pages/home"
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Sign_in from 'pages/sign_in'
-import Sign_up from 'pages/sign_up'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./index.css";
+import store from "./store-redux/store.js";
+import { Provider } from "react-redux";
+import Home from "pages/home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Sign_in from "pages/sign_in";
+import Sign_up from "pages/sign_up";
 
 const App = () => (
-<Router>
-  <Navbar/>
-  <Switch>
-    <Route path="/" exact>
-      <Home />
-    </Route>
-    <Route path="/sign_in">
-      <Sign_in />
-    </Route>
-    <Route path="/sign_up">
-      <Sign_up />
-    </Route>
-  </Switch>
- <Footer/>
-</Router>
-
+  <Router>
+    <Provider store={store}>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/sign_in">
+          <Sign_in />
+        </Route>
+        <Route path="/sign_up">
+          <Sign_up />
+        </Route>
+      </Switch>
+      <Footer />
+    </Provider>
+  </Router>
 );
 
 ReactDOM.render(<App />, document.getElementById("root"));
