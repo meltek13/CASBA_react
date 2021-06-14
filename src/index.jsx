7ddb965d2003dbff5f60b4276cab86ed137eaef7
meltek13 <regnault.melvin@hotmail.fr>
2021-06-14 10:09:54 +0200

    start: try to fix for everybody

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import {
     Route,
     Switch,
   } from 'react-router-dom';
-import './index.scss'
+import './index.css'
 import Home from "pages/home"
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
