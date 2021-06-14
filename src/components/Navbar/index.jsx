70037a11a3882e37b750b19fc7df515671d125a8
meltek13 <regnault.melvin@hotmail.fr>
2021-06-14 11:13:16 +0200

    ADD: sign in sign up , some bug with disconnect to fix

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,12 +2,43 @@ import React from 'react'
 import {
   Link,
 } from 'react-router-dom';
+import Cookies from "js-cookie";
+
 const Navbar = () => {
+  //const dispatch = useDispatch();
+  //const loged = useSelector((state) => state.loged);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+ 
+    fetch("http://localhost:3000/users/sign_out", {
+        method: "delete",
+        headers: {
+          Authorization: Cookies.get("token"),
+          "Content-Type": "application/json",
+        }, 
+      })
+        .then((response) => response.json())
+        .then((userdata) => {
+          console.log(userdata)
+          Cookies.remove("token");
+          //dispatch(logOut());
+        });
+  };
 
   return(
     <div className="navbar">
       <div className="content-link">
         <Link className="link" to="/">Home</Link>
+      </div>
+      <div className="content-link">
+        <Link className="link" to="/sign_in">Se connecter</Link>
+      </div>
+      <div className="content-link">
+        <Link className="link" to="/sign_up">S'inscrire</Link>
+      </div>
+      <div className="content-link">
+        <a href="" onClick={handleClick}>Se deconnecter</a>
       </div>
     </div>
   )
