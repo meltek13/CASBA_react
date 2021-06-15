193d8ee28db90d1814c2f362ee6b0f10e4044a1f
Kelvi3 <theocazenavecoupet@gmail.com>
2021-06-15 16:59:17 +0200

    ADD: new pages for dashboard

---
+++ src/pages/expense/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Expense = () => {
+
+  return(
+    <h1>Depense</h1>
+  )
+}
+
+
+export default Expense
---
+++ src/pages/picture/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Picture = () => {
+
+  return(
+    <h1>Photos</h1>
+  )
+}
+
+
+export default Picture
