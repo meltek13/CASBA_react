dbd21053eda6243b429d3a221c046471440ab9e4
frederic <fredericseguier@yahoo.fr>
2021-06-15 10:17:28 +0200

    ADD: edit email profil

--- src/index.jsx
+++ src/index.jsx
@@ -1,53 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import './index.css'
+import "./index.css";
 import "antd/dist/antd.css";
-import Home from "pages/home"
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Sign_in from 'pages/sign_in'
-import Sign_up from 'pages/sign_up'
-import NewFlatSharing from 'pages/new_flatSharing'
+import Home from "pages/home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Sign_in from "pages/sign_in";
+import Sign_up from "pages/sign_up";
+import NewFlatSharing from "pages/new_flatSharing";
 import Profil from "./pages/profil";
+import EditProfil from "pages/editProfil";
 import store from "./store-redux/store.js";
 import { Provider } from "react-redux";
-import Calendar from './pages/calendar'
-
-
+import Calendar from "./pages/calendar";
 
 const App = () => (
-
-
   <div id="app-container">
     <Router>
-        <Provider store={store}>
-          <Navbar />
-          <Switch>
-            <Route path="/" exact>
-              <Home />
-            </Route>
-            <Route path="/sign_in">
-              <Sign_in />
-            </Route>
-            <Route path="/sign_up">
-              <Sign_up />
-            </Route>
-            <Route path="/profil">
-              <Profil />
-            </Route>
-           <Route path="/new_flat_sharing">
-             <NewFlatSharing />
-           </Route>
+      <Provider store={store}>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/sign_in">
+            <Sign_in />
+          </Route>
+          <Route path="/sign_up">
+            <Sign_up />
+          </Route>
+          <Route path="/profil">
+            <Profil />
+          </Route>
+          <Route path="/edit_profil">
+            <EditProfil />
+          </Route>
+          <Route path="/new_flat_sharing">
+            <NewFlatSharing />
+          </Route>
           <Route path="/calendar">
             <Calendar />
           </Route>
-          </Switch>
-          <Footer />
-        </Provider>
+        </Switch>
+        <Footer />
+      </Provider>
     </Router>
   </div>
-
 );
 
 ReactDOM.render(<App />, document.getElementById("root"));
