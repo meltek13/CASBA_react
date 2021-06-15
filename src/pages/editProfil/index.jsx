c495296b012553ad0c7ef8b2e8726db179027a7b
frederic <fredericseguier@yahoo.fr>
2021-06-15 15:23:36 +0200

    UPDATE: upload avatar

--- src/pages/editProfil/index.jsx
+++ src/pages/editProfil/index.jsx
@@ -1,24 +1,28 @@
 import React, { useState } from "react";
 import Cookies from "js-cookie";
 import ButtonUpdate from "../../components/ButtonUpdate";
+import { useHistory } from "react-router-dom";
 
 const EditProfil = () => {
   const [email, setEmail] = useState("");
-  const [avatar, setAvatar] = useState("");
+  const history = useHistory();
 
   const update = (e) => {
     e.preventDefault();
+
     const formData = new FormData();
+
     formData.append("email", email);
-    formData.append("avatar", avatar);
 
     fetch("http://localhost:3000/members/" + Cookies.get("current_user_id"), {
       method: "PUT",
-
       body: formData,
     })
       .catch((error) => console.log(error))
-      .then((response) => console.log(response));
+      .then((response) => {
+        console.log(response);
+        history.push("/profil");
+      });
   };
 
   return (
@@ -29,12 +33,7 @@ const EditProfil = () => {
           onChange={(event) => setEmail(event.target.value)}
           placeholder="Email"
         />
-        <input
-          type="file"
-          accept="image/*"
-          multiple={false}
-          onChange={(event) => setAvatar(event.target.files[0])}
-        />
+
         <ButtonUpdate action={update} name="Modifier mon profil" />
       </form>
     </div>
