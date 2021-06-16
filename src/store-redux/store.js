ab70d4803902fa455965fba7bec0e98ab35bb35a
frederic <fredericseguier@yahoo.fr>
2021-06-16 12:18:20 +0200

    ADD: combineReducers

--- src/store-redux/store.js
+++ src/store-redux/store.js
@@ -1,9 +1,15 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers } from "redux";
 import userReducer from "./usersLog/userReducer";
+import flatReducer from "./flatLog/flatReducer";
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  flat: flatReducer,
+});
 
 const Store = createStore(
-    userReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  );
-  
-  export default Store;
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default Store;
