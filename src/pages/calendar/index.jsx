258531022c43b3ae05cb10007216a479bd82e948
Kelvi3 <theocazenavecoupet@gmail.com>
2021-06-17 15:24:40 +0200

    UPDATE: view a creat of event

--- src/pages/calendar/index.jsx
+++ src/pages/calendar/index.jsx
@@ -3,6 +3,8 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import './calendar.css'
 import { useParams } from "react-router-dom";
+import Cookies from "js-cookie";
+
 const Timetable = () => {
   
   const { id } = useParams()
@@ -12,6 +14,7 @@ const Timetable = () => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [event, setEvent] = useState([]);
+  const [userCreate, setUserCreate] = useState([])
 
   const changePopUp = () => {
     setPopUp(true)
@@ -23,15 +26,26 @@ const Timetable = () => {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({"title": title, "description": description, "timedate": value.toString(), "dashboard": id})
+      body: JSON.stringify({"title": title, "description": description, "timedate": value.toString(), "dashboard": id, "by": parseInt(Cookies.get("current_user_id"))})
     })
   }
+
+
+
 
   useEffect(() => {
     fetch("http://localhost:3000//dashboard/" + id + "/calendars")
       .then((response) => response.json())
       .then((response) => setEvent(response))
   }, [])
+
+  useEffect(() => {
+    
+    fetch("http://localhost:3000/members/" + event.map(e => e.by))
+    .then((response) => response.json())  
+    .then((response) => setUserCreate(oldArray => [...oldArray, response]))
+   ;
+  }, [event])
 
   const destroyEvent = (id) => {
     fetch('http://localhost:3000/calendars/' + id,{
@@ -40,6 +54,7 @@ const Timetable = () => {
     window.location.reload(false);
   }
 
+  console.log()
   return (
     <>
       <div className="content-calendar">
@@ -56,6 +71,9 @@ const Timetable = () => {
           <div className="popUp-container">
             {event.map(elem => elem.timedate === value.toString() && event.length > 0 &&
               <div className="contain-text" key={elem.id}>
+                {userCreate[0]?.users.map(user => 
+                  user.id === elem.by && <p>de: {user.email}</p>
+                )}
                 <p className="text-pop">Titre:{elem.title}</p>
                 <p className="text-pop">Description: {elem.description}</p>
                 <button className="button-delete" onClick={() =>  destroyEvent(elem.id)}>Supprimer l'evenement</button>
