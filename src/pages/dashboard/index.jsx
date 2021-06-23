b3dbe054366dcef5df33ef1b6da6450d1e779a4b
frederic <fredericseguier@yahoo.fr>
2021-06-23 21:29:36 +0200

    Correction

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -12,9 +12,8 @@ import url from "data/url.json";
 import { ArrowRightOutlined, CheckCircleOutlined } from "@ant-design/icons";
 import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
-import PageExpense from 'pages/display_expense';
-import Error403 from "pages/error403"
-
+import PageExpense from "pages/display_expense";
+import Error403 from "pages/error403";
 
 const Dashboard = () => {
   const { id } = useParams();
@@ -75,39 +74,32 @@ const Dashboard = () => {
     fetch(url.url + "flatsharings/" + id + "/dashboard")
       .then((response) => response.json())
       .then((response) => {
-          Cookies.set("admin_email", response.admin.email);
-          setRoom(response);
+        Cookies.set("admin_email", response.admin.email);
+        setRoom(response);
       });
   };
-
 
   const ItisYourDashboard = () => {
     fetch(url.url + "flatsharings/" + id + "/dashboard")
       .then((response) => response.json())
       .then((response) => {
-
-          if(Cookies.get("current_user_id")) {
-            if (parseInt(Cookies.get("current_user_id")) === response.admin.id){
-              console.log(response.admin.id)
-              setYourDashboard(true)
-            } 
-            else {
-              response.guest.forEach((flatmate)=>{
-                if (flatmate !== null){
-                  if (flatmate.id === parseInt(Cookies.get("current_user_id"))){
-                    console.log(flatmate)
-                    setYourDashboard(true)
-                  }
+        if (Cookies.get("current_user_id")) {
+          if (parseInt(Cookies.get("current_user_id")) === response.admin.id) {
+            console.log(response.admin.id);
+            setYourDashboard(true);
+          } else {
+            response.guest.forEach((flatmate) => {
+              if (flatmate !== null) {
+                if (flatmate.id === parseInt(Cookies.get("current_user_id"))) {
+                  console.log(flatmate);
+                  setYourDashboard(true);
                 }
-              })  
-            }
+              }
+            });
+          }
         }
-        
-        
-       
       });
   };
-
 
   useEffect(() => {
     ItisYourDashboard();
@@ -116,89 +108,98 @@ const Dashboard = () => {
 
   return (
     <div>
-      {yourDashboard? 
- <div>
-      <div className="nav-dashboard">
-      <button onClick={changeNews} className="btn-dashboard-nav">
-        <span>Actus</span>
-        <strong>📰</strong>
-      </button>
-      <button onClick={changePicture} className="btn-dashboard-nav">
-        <strong>📷</strong>
-        <span>Photos</span>
-      </button>
-      <button onClick={changeCalendar} className="btn-dashboard-nav">
-        <strong>🗓️</strong>
-        <span>Calendrier</span>
-      </button>
-      <button onClick={changeExpenses} className="btn-dashboard-nav">
-        <strong>💰</strong>
-        <span>Dépenses</span>
-      </button>
-    </div>
-    <hr/>
-
+      {yourDashboard ? (
+        <div>
+          <div className="nav-dashboard">
+            <button onClick={changeNews} className="btn-dashboard-nav">
+              <span>Actus</span>
+              <strong>📰</strong>
+            </button>
+            <button onClick={changePicture} className="btn-dashboard-nav">
+              <strong>📷</strong>
+              <span>Photos</span>
+            </button>
+            <button onClick={changeCalendar} className="btn-dashboard-nav">
+              <strong>🗓️</strong>
+              <span>Calendrier</span>
+            </button>
+            <button onClick={changeExpenses} className="btn-dashboard-nav">
+              <strong>💰</strong>
+              <span>Dépenses</span>
+            </button>
+          </div>
+          <hr />
 
           <div className="Mini_avatar_display rightSide">
-            <div className="StatusSolde">
-              <h3>Colloc</h3>
-              <h3>Status</h3>
-              <h3>Solde</h3>
-             
+            <div className="scroll-colloc">
+              <div className="StatusSolde">
+                <div>Colloc</div>
+                <div className="alignStatus"> Status</div>
+                <div>Solde</div>
               </div>
-             
+              <hr />
+              {room.admin && (
+                <MiniAvatar user={room.admin} key={room.admin.id} />
+              )}
 
-{ room.admin && < MiniAvatar user={room.admin} key={room.admin.id}/> }
-               
-              {room?.guest?.map(user => 
-                  verifyPresenceOfData(user)? 
-                      <div>
-                      < MiniAvatar user={user} key={user.id}/>
-                      </div>
-                          :
-                          <div className="unsubscribe">
-                      <Popover placement="leftBottom" content={"non inscrit"}>
-                          <label for="file">
-                              <img
-                                 style={{border:"3px solid #FFFFB9"}}
-                              className="avatar_dashboard"
-                              src="https://oasys.ch/wp-content/uploads/2019/03/photo-avatar-profil.png"
-                              alt="avatar"
-                              />
-                          </label>
-                      </Popover>
-                        <PageExpense/>
-                      </div>
-              )} 
-              
-           
-            {parseInt(Cookies.get("current_user_id")) === room?.admin?.id && 
-            
-            <div className="add-guest">
-              <div className="add-Room-mate">
-              <Link  to={'/add-room-mate/' + id }>
-                <Popover placement="leftBottom" content="Ajouter un collocataire">
-                  <label htmlFor="file">
-                    <img className="avatar_dashboard"  src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/OOjs_UI_icon_add.svg/1200px-OOjs_UI_icon_add.svg.png" alt="avatar" />
-                  </label>
-                </Popover>
-              </Link>
-              <Link  to={'/add-room-mate/' + id }>Ajouter un colloc</Link>
-              </div>
+              {room?.guest?.map((user) =>
+                verifyPresenceOfData(user) ? (
+                  <div>
+                    <MiniAvatar user={user} key={user.id} />
+                  </div>
+                ) : (
+                  <div className="unsubscribe">
+                    <Popover placement="leftBottom" content={"non inscrit"}>
+                      <label for="file">
+                        <img
+                          style={{ border: "3px solid #FFFFB9" }}
+                          className="avatar_dashboard"
+                          src="https://oasys.ch/wp-content/uploads/2019/03/photo-avatar-profil.png"
+                          alt="avatar"
+                        />
+                      </label>
+                    </Popover>
+                  </div>
+                )
+              )}
             </div>
-          }
+            {parseInt(Cookies.get("current_user_id")) === room?.admin?.id && (
+              <div>
+                <div className="add-guest">
+                  <div className="add-Room-mate">
+                    <Link to={"/add-room-mate/" + id}>
+                      <Popover
+                        placement="leftBottom"
+                        content="Ajouter un collocataire"
+                      >
+                        <label htmlFor="file">
+                          <img
+                            className="avatar_dashboard"
+                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/OOjs_UI_icon_add.svg/1200px-OOjs_UI_icon_add.svg.png"
+                            alt="avatar"
+                          />
+                        </label>
+                      </Popover>
+                    </Link>
+                    <Link to={"/add-room-mate/" + id}>Ajouter un colloc</Link>
+                  </div>
+                </div>
+                <PageExpense />
+              </div>
+            )}
           </div>
-    <div className="content-dashboard">
-      {news && <News />}
-      {picture && <Picture />}
-      {calendar && <Calendar />}
-      {expense && <Expense />}
+          <div className="content-dashboard">
+            {news && <News />}
+            {picture && <Picture />}
+            {calendar && <Calendar />}
+            {expense && <Expense />}
+          </div>
+          <div />
+        </div>
+      ) : (
+        <Error403 />
+      )}
     </div>
-    <div />
-    <h2 id="title-expense-col">Dépenses</h2>
-  </div>
-  : <Error403 />}
-  </div>      
   );
 };
 
