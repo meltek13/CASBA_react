1c001aacc0ac83efdc7a146c40dbe9aae211a071
meltek13 <regnault.melvin@hotmail.fr>
2021-06-24 20:10:01 +0200

    update : fixed bug

--- src/pages/expense/index.jsx
+++ src/pages/expense/index.jsx
@@ -88,7 +88,7 @@ const Expense = () => {
       .then((response) => response.json())
       .then((response) =>  {
            updateSoldePositif(response)
-           updateSoldeNegatif(response)
+           
         // if (response.ok == true) {
         //   Notif_sucess_expense()
         // } else{
@@ -112,8 +112,8 @@ const deleteFlatMate = (flatmate) =>{
     
 
 }
-console.log(concernedColocsInput2.length)
-console.log(concernedColocsInput)
+
+
 
   const updateSoldePositif = (expense) => {
 
@@ -126,30 +126,25 @@ console.log(concernedColocsInput)
     })
       .then((response) => response.json())
       .then((response) => {
-        let amount = response.solde + expense.expense.total_amount;
-
-      expense.expense.concerned_colocs.forEach(x=>{
-       if (x === expense.expense.user_id){
-         let amount = response.solde + expense.expense.split_amount_to_colocs
-       } 
-       })
         
-       
+
     const formData = new FormData();
-    formData.append("solde", amount );
+
+   formData.append("solde", response.solde + expense.expense.total_amount );
+   
     fetch(url.url + "members/" + expense.expense.user_id, {
       method: "PUT",
       body: formData,
     })
       .catch((error) => console.log(error))
       .then((response) => {
-        console.log(response);
+        updateSoldeNegatif(expense)
       });
     });
   };
 
   const updateSoldeNegatif = (expense) => {
-    console.log(expense.expense)
+    
     expense.expense.concerned_colocs.forEach( colloc_id => {
       fetch(url.url + "members/" + colloc_id, {
         method: "get",
@@ -180,85 +175,8 @@ console.log(concernedColocsInput)
     <>
       <div id="container_expense">
         <img id="expense-svg" src={Expense_svg} alt="illustration tirelire cochon " />
-        <h2>Crées un dépense liée à la colocation</h2>
-        <div id="form-action">
-          <input className="c-checkbox" type="checkbox" id="start"/>
-          <input className="c-checkbox" type="checkbox" id="progress2"/>
-          <input className="c-checkbox" type="checkbox" id="progress3"/>
-          <input className="c-checkbox" type="checkbox" id="finish"/>
-          <div className="c-form__progress"></div>
-
-            <div className="c-formContainer">
-              {/* <div className="c-welcome">Welcome aboard!</div> */}
-              <form className="c-form" action="">
-                <div className="c-form__group">
-                  <label className="c-form__label" htmlFor="title-expense">
-                                <input
-                                    type="text"
-                                    id="username"
-                                    className="c-form__input"
-                                    placeholder=" "
-                                    pattern="[a-zA-Z0-9\s]+"
-                                    onChange= {onChangetitle}
-                                    required
-                                    value={titleInput}/>
-
-                                <label className="c-form__next" htmlFor="progress2" role="button">
-                                    <span className="c-form__nextIcon"></span>
-                                </label>
-
-                  <span className="c-form__groupLabel">Titre de la dépense</span>
-                  <b className="c-form__border"></b>
-                  </label>
-                </div>
-
-                <div className="c-form__group">
-                  <label className="c-form__label" htmlFor="total_amount">
-                                <input
-                                    type="text"
-                                    id="username"
-                                    className="c-form__input"
-                                    placeholder=" "
-                                    pattern="[0-9]{1,5}"
-                                    onChange = {onChangeTotalAmount}
-                                    value={totalAmountInput}
-                                    required/>
-
-                                <label className="c-form__next" htmlFor="progress3" role="button">
-                                    <span className="c-form__nextIcon"></span>
-                                </label>
-
-                  <span className="c-form__groupLabel">Total de la dépense</span>
-                  <b className="c-form__border"></b>
-                  </label>
-                </div>
-
-                <div className="c-form__group">
-                  <label className="c-form__label" htmlFor="concerned_colocs">
-                                <input
-                                    type="number"
-                                    id="username"
-                                    className="c-form__input"
-                                    placeholder=" "
-                                    pattern="^[0-9]{1,10}"
-                                    onChange={onChangeConcernedColocs}
-                                    value={concernedColocsInput}
-                                    required/>
-                 
-                    <input id="btn-submit-expense" type="submit" value="créer" onClick={ExpenseFetch}/>
-                  
-                  <span className="c-form__groupLabel">Colocs concernées</span>
-                  <b className="c-form__border"></b>
-                  </label>
-                </div>
-
-               <label className="c-form__toggle" htmlFor="start">Créer une Dépense</label>
-
-              </form>
-              
-            </div>
-          </div>
-      </div>
+       
+     
 
       <Form {...layout}  name="control-ref" >
         <span>Intitulé de la dépense ?</span>
@@ -357,7 +275,7 @@ console.log(concernedColocsInput)
           </Button>
         </Form.Item>
       </Form>
-
+      </div>
     </>
   )
 };
