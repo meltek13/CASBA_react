0ec435485d28bc01bacda6ee60dda43f4cacfe53
meltek13 <regnault.melvin@hotmail.fr>
2021-06-25 11:43:50 +0200

    update: delete colloc

--- src/pages/addRoomMate/index.jsx
+++ src/pages/addRoomMate/index.jsx
@@ -1,15 +1,28 @@
 import React, { useState } from 'react';
 import { ArrowRightOutlined, CheckCircleOutlined } from '@ant-design/icons';
-import { Popover, Button, Input } from 'antd';
+import { Button, Input, Modal } from 'antd';
 import { useHistory, useParams } from 'react-router-dom';
-
+import Cookies from 'js-cookie';
 import url from 'data/url.json';
 import './addRoomMate.css';
+import { disconnect } from 'store-redux/room';
+import { useDispatch } from 'react-redux';
 
 const AddRoomMate = () => {
   const history = useHistory();
   const [guest, setGuest] = useState('');
   const { id } = useParams();
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const dispatch = useDispatch();
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
 
   const addGuest = () => {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -31,6 +44,24 @@ const AddRoomMate = () => {
         });
     }
   };
+
+  const deleteFlatsharing = () => {
+    fetch(`${url.url}flatsharings/${id}`  , {
+      method: 'delete',
+      headers: {
+        Authorization: Cookies.get('token'),
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        setIsModalVisible(false);
+        Cookies.remove('flat_id');
+        dispatch(disconnect());
+        history.push("/")
+      });
+  };
+
   return (
     <div>
       <h1>Invitation</h1>
@@ -41,6 +72,12 @@ const AddRoomMate = () => {
           <ArrowRightOutlined />
         </Button>
       </form>
+      <Button type="text" onClick={showModal} danger>
+      Supprimer la collocation ?
+    </Button>
+      <Modal title="Supprimer la colloc" visible={isModalVisible} onOk={deleteFlatsharing} onCancel={handleCancel}>
+        <p>Cette action est irréversible, est-tu sûre de vouloir supprimer la collocation ?</p>
+      </Modal>
     </div>
 
   );
